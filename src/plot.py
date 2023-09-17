85b2104110fa1435b9eb683759aae2a6dd005d49
MartinPoveda <martin.poveda.hci@gmail.com>
2023-09-17 10:57:42 +0200

    Moved notebook to notebooks folder and add parameter to save function

--- src/plot.py
+++ src/plot.py
@@ -19,23 +19,43 @@ def _generic_bar_plot(x,y, figure_type=None):
         correlation_figure()
     plt.bar(x, y)
 
-def plot_chain(df, col):
-    # Plot
+def per_time_plot(df, col, save_img_path):
     _generic_bar_plot(df.index, df[col], figure_type='temporal')
     plt.title(f'{col} per time')
-    # Plot
+    plt.savefig(f'{save_img_path}/{col} per time.png', dpi=800)
+
+def per_post_plot(df, col, save_img_path):
     _generic_bar_plot(df['post_number'], df[col], figure_type='temporal')
     plt.title(f'{col} per post')
-    # Plot
-    reach_corr = df.corr()[col].drop(col).dropna().sort_values(ascending=False)
-    _generic_bar_plot(reach_corr.index, reach_corr, figure_type='correlation')
+    plt.savefig(f'{save_img_path}/{col} per post.png', dpi=800)
+
+def corr_plot(df, col, save_img_path):
+    corr = df.corr()[col].drop(col).dropna().sort_values(ascending=False)
+    _generic_bar_plot(corr.index, corr, figure_type='correlation')
     plt.title(f'{col} correlation')
     plt.xticks(rotation=90)
-    # Plot
+    plt.savefig(f'{save_img_path}/{col} correlation.png', dpi=800)
+
+def per_hour_plot(df, col, save_img_path):
     hourly_df = df.groupby(df.index.hour).mean()
     _generic_bar_plot(hourly_df.index, hourly_df[col], figure_type='correlation')
     plt.title(f'{col} for each hour')
-    # Plot
+    plt.savefig(f'{save_img_path}/{col} for each hour.png', dpi=800)
+
+def per_week_plot(df, col, save_img_path):
     weekly_df = df.groupby(df.index.day_of_week).mean()
     _generic_bar_plot(weekly_df.index, weekly_df[col], figure_type='correlation')
     plt.title(f'{col} for each day of the week')
+    plt.savefig(f'{save_img_path}/{col} for each day of the week.png', dpi=800)
+
+def plot_chain(df, col, save_img_path=None):
+    # Plot per time
+    per_time_plot(df, col, save_img_path)
+    # Plot per post
+    per_post_plot(df, col, save_img_path)
+    # Correlation plot
+    corr_plot(df, col, save_img_path)
+    # Hourly plot
+    per_hour_plot(df, col, save_img_path)
+    # Plot
+    per_week_plot(df, col, save_img_path)
