dd1d3a519591114e05e28493fe559f01700b522f
MartinPoveda <martin.poveda.hci@gmail.com>
2023-09-18 20:37:05 +0200

    Add moving average to plots

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This repository is meant to analysis results of a communication campaign.
 
 ## Installation
 
- 1 - Intsall Git Bash and Anaconda if not already done<br>
+ 1 - Install Git Bash and Anaconda if not already done<br>
  2 - Use git clone <url> in the desired folder or download the code<br>
  3 - Open the notebook you want with your Jupyter editor<br>
  Note: notebook paths current directory is supposed to be the root path of this project.<br>
--- src/plot.py
+++ src/plot.py
@@ -12,39 +12,55 @@ def temporal_figure():
 def correlation_figure():
     _generic_figure((10,6))
 
-def _generic_bar_plot(x,y, figure_type=None):
+def fig(figure_type=None):
     if figure_type=='temporal':
         temporal_figure()
     if figure_type=='correlation':
         correlation_figure()
-    plt.bar(x, y)
+
+def _generic_plot(x,y, plot_type=None):
+    if plot_type=='bar':
+        plt.bar(x, y)
+    if plot_type=='plot':
+        plt.plot(x, y, color='red')
 
 def per_time_plot(df, col, save_img_path):
-    _generic_bar_plot(df.index, df[col], figure_type='temporal')
+    fig(figure_type='temporal')
+    _generic_plot(df.index, df[col], plot_type='bar')
+    moving_average = df.rolling(7).mean()
+    _generic_plot(df.index, moving_average[col], plot_type='plot')
     plt.title(f'{col} per time')
     plt.savefig(f'{save_img_path}/{col} per time.png', dpi=800)
 
 def per_post_plot(df, col, save_img_path):
-    _generic_bar_plot(df['post_number'], df[col], figure_type='temporal')
+    fig(figure_type='temporal')
+    _generic_plot(df['post_number'], df[col], plot_type='bar')
+    moving_average = df.rolling(7).mean()
+    _generic_plot(df['post_number'], moving_average[col], plot_type='plot')
     plt.title(f'{col} per post')
     plt.savefig(f'{save_img_path}/{col} per post.png', dpi=800)
 
 def corr_plot(df, col, save_img_path):
     corr = df.corr()[col].drop(col).dropna().sort_values(ascending=False)
-    _generic_bar_plot(corr.index, corr, figure_type='correlation')
+    fig(figure_type='correlation')
+    _generic_plot(corr.index, corr, plot_type='bar')
+    moving_average = corr.rolling(7).mean()
+    _generic_plot(corr.index, moving_average, plot_type='plot')
     plt.title(f'{col} correlation')
     plt.xticks(rotation=90)
     plt.savefig(f'{save_img_path}/{col} correlation.png', dpi=800)
 
 def per_hour_plot(df, col, save_img_path):
     hourly_df = df.groupby(df.index.hour).mean()
-    _generic_bar_plot(hourly_df.index, hourly_df[col], figure_type='correlation')
+    fig(figure_type='correlation')
+    _generic_plot(hourly_df.index, hourly_df[col], plot_type='bar')
     plt.title(f'{col} for each hour')
     plt.savefig(f'{save_img_path}/{col} for each hour.png', dpi=800)
 
 def per_week_plot(df, col, save_img_path):
     weekly_df = df.groupby(df.index.day_of_week).mean()
-    _generic_bar_plot(weekly_df.index, weekly_df[col], figure_type='correlation')
+    fig(figure_type='correlation')
+    _generic_plot(weekly_df.index, weekly_df[col], plot_type='bar')
     plt.title(f'{col} for each day of the week')
     plt.savefig(f'{save_img_path}/{col} for each day of the week.png', dpi=800)
 
